7bae87a8e61876cd3a7b2e4de141d1e4496284bc
Edgar Z. Alvarenga <edgar@ymonad.com>
2012-08-30 14:57:36 -0300

    Histogram and vector hash working

--- stochastics/main.hs
+++ stochastics/main.hs
@@ -4,6 +4,7 @@ import System.Random.MWC
 import qualified System.Random as R 
 import qualified Data.Vector.Unboxed as V
 import Control.Monad (replicateM)
+import qualified Data.Map as M
 
 data RandVec = RV { dim  :: Int
                   , mod  :: Int
@@ -29,14 +30,27 @@ changeRandVec rv@(RV _ !m !bds !v) g = do
     r_i <- uniformR (0, sum_ij) g
     return $ rv{ vals = v V.// [ (i, r_i), (j, sum_ij - r_i) ]}
 
-vec_hash (RV _ m _ v) = V.sum $ V.zipWith (*) (V.map (m^) (V.enumFromTo 0 (V.length v - 1))) v
+vec_hash :: RandVec -> Integer
+vec_hash (RV _ m _ v) = sum $ zipWith (*) (map (mi^) [0.. lv]) ivec
+    where 
+        mi = fromIntegral m
+        lv = fromIntegral $ V.length v - 1
+        ivec = map fromIntegral $ V.toList v
 
+save_points l t = writeFile filename $ unlines out
+   where
+       out = map (\(i,j) -> (show i) ++ ", " ++ (show j)) $
+                zip [1..] $ map ((/(fromIntegral t)).fromIntegral) l
+       filename = "points-" ++ (show t) ++ ".csv"
+ 
 main = do
    sg <- R.getStdGen
    rs <- return $ take 256 $ R.randoms sg
    g <- initialize $ V.fromList rs
    rv <- makeRandVec 10 (0,2) g
-   let num_vecs = 10000
+   let num_vecs = 1000000
    new_vecs <- replicateM num_vecs (changeRandVec rv g)
+   let hist = M.fromListWith (+) $ zip (map vec_hash new_vecs) (repeat 1)
+   save_points (M.elems hist) num_vecs
    return ()
  
