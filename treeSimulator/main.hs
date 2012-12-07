e43142a337c63ce706d9f094526c69056d36b3ae
Edgar Z. Alvarenga <edgar@ymonad.com>
2012-12-07 15:25:05 -0200

    Adding code to generate random trees and sequences

--- treeSimulator/main.hs
+++ treeSimulator/main.hs
@@ -1,6 +1,7 @@
 import qualified Data.IntMap as M
 import Data.List (find, sortBy)
 import System.Random.Mersenne as R
+import Control.Monad (forM_)
 
 type Symbol = Int
 type ProbS = [(Double, Symbol)]
@@ -10,25 +11,6 @@ data Tree = Branch Symbol Tree Tree
           | Node Symbol Double
           deriving (Show, Read)
  
-sortProbs :: [(Double, Symbol)] -> [(Double, Symbol)]
-sortProbs = sortBy (\(x,_) (y,_) -> compare x y)
-
-binM p0 = sortProbs [(p0, 0), (1, 1)]
-
-t = Root (Branch 1 (Branch 1 (Node 1 0.2) 
-                             (Node 0 0.7))
-                   (Node 0 0))
-         (Branch 0 (Branch 1 (Node 1 0.6) 
-                             (Node 0 0.7))
-                   (Node 0 0))
-
-t2 = Root (Branch 1 (Branch 1 (leftRoot t)
-                              (Node 0 0.7))
-                    (Node 0 0))
-          (Branch 0 (Branch 1 (leftRoot t) 
-                              (Node 0 0.7))
-                    (rightRoot t))
-
 randTree max_depth g = do
     left <- rt max_depth 0 g
     right <- rt max_depth 1 g
@@ -45,19 +27,6 @@ randTree max_depth g = do
                   else do 
                     return.(Node b) =<< random g
 
-randInit tree@(Root l r) g = do
-    isLeft <- random g :: IO Bool
-    if isLeft
-      then ri [] l g
-      else ri [] r g
-    where
-        ri seq (Node b p) g = return.reverse $ b:seq
-        ri seq (Branch b l p) g = do
-            isLeft <- random g
-            if isLeft
-              then ri (b:seq) l g
-              else ri (b:seq) r g
- 
 genSeq :: MTGen -> Int -> Tree -> [Int] -> IO [Int]
 genSeq rg size tree initSeq = gs size initSeq
     where
@@ -83,12 +52,14 @@ newSymbol rg (Root t1 t2) seq = walkTree seq t1 t2
                        then return $ 0:seq
                        else return $ 1:seq
                        
-pl :: [(Double, Int)]
-pl = [(0.7, 3), (0.85, 2), (1, 1)]
-
-randW :: [(Double, Int)] -> MTGen -> IO Int
-randW pl rg = do
-    r <- random rg
-    case find ((>= r).fst) pl of
-        Nothing -> error "Nope"
-        Just (p, x) -> return x
+main = do
+    g <- getStdGen
+    forM_ [100, 1000, 10000, 100000, 1000000] $ \steps -> do
+        forM_ [10, 20..100] $ \treeDepth -> do
+           forM_ [1..10] $ \i -> do
+               randomTree <- randTree treeDepth g
+               randInit <- return.(map (\x -> if x then 1 else 0)).(take (100)) =<< randoms g
+               dataSeq <- genSeq g steps randomTree randInit 
+               let treeFile = "tree-" ++ (show treeDepth) ++ "-" ++ (show i) ++ "-" ++ (show steps) 
+               writeFile treeFile (show randomTree)
+               writeFile (treeFile ++ ".data") (show dataSeq)
