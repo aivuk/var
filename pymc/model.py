964920618c0aac67ecd031a56cc96ab9ad5c2994
Edgar Z. Alvarenga <edgar@ymonad.com>
2013-05-22 19:46:44 -0300

    Drawing the model graph

--- pymc/model.py
+++ pymc/model.py
@@ -6,8 +6,8 @@ data = np.array([0 for i in range(0,500)] + [1] + [2 for i in range(0, 500)])
 c0 = len(data[data == 0])
 c1 = len(data[data == 1])
 c2 = len(data[data == 2])
-
-p = pymc.Dirichlet('prior', theta=[c0, c1, c2])
+                                            
+p = pymc.Dirichlet('prior', theta=[len(data)/3, len(data)/3, len(data)/3])
 choices = pymc.Categorical('choice', p=p, value=data, observed=True)
 choices_sim = pymc.Categorical('choice_sim', p=p) 
 
