5e215f6d54a3972254cef6c131dd40f9941347bc
Edgar Z. Alvarenga <edgar@ymonad.com>
2013-05-23 00:05:44 -0300

    Using multinomial in the tests

--- pymc/model.py
+++ pymc/model.py
@@ -1,13 +1,29 @@
 import pymc
 import numpy as np
 
-data = np.array([0 for i in range(0,500)] + [1] + [2 for i in range(0, 500)])
+n_ind = 100
+trials_per_session = 100
+sessions = 30
 
-c0 = len(data[data == 0])
-c1 = len(data[data == 1])
-c2 = len(data[data == 2])
-                                            
-p = pymc.Dirichlet('prior', theta=[len(data)/3, len(data)/3, len(data)/3])
-choices = pymc.Categorical('choice', p=p, value=data, observed=True)
-choices_sim = pymc.Categorical('choice_sim', p=p) 
+data = []
+
+for i in xrange(0, n_ind):
+    data.append(np.random.multinomial(trials_per_session, [1/3.]*3, size=sessions))
+
+ind_params = []
+choices = []
+
+for i in xrange(0, n_ind):                                            
+    ind_params.append([])
+    choices.append([])
+    for s in xrange(0, sessions):
+        ind_params[-1].append(
+                pymc.Dirichlet('prior_ind_{}_session_{}'.format(i,s),
+                               theta=[1., 1., 1.]))
+
+        choices[-1].append(pymc.Multinomial('choice_ind_{}_session_{}'.format(i,s), 
+                                            n=trials_per_session, 
+                                            p=ind_params[-1][-1],
+                                            value=data[i][s], 
+                                            observed=True))
 
