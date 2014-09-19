20d84113e3da2f91f14ae3a155a569844fd75f73
Edgar Z. Alvarenga <edgar@ymonad.com>
2014-09-19 05:34:40 +0400

    Organizing the code

--- pattern_recognition/find_genes.py
+++ pattern_recognition/find_genes.py
@@ -3,37 +3,35 @@ import numpy as np
 import pickle
 from cvxopt import matrix, solvers
 
-data = pd.read_csv('./data_breast_cancer.csv', header=False)
-da = data.T
-
-lin_sep = []
-braca1 = da[da[3226] == 'BRACA1']
-braca2 = da[da[3226] == 'BRACA2']
-not_braca1 = da[da[3226] != 'BRACA1'][1:]
-not_braca2 = da[da[3226] != 'BRACA2'][1:]
+data = pd.read_csv('./data_breast_cancer.csv', header=False).T
+braca1, braca2 = data[data[3226] == 'BRACA1'], data[data[3226] == 'BRACA2']
+not_braca1, not_braca2 = data[data[3226] != 'BRACA1'][1:], data[data[3226] != 'BRACA2'][1:]
 n_genes = 3226
+groups = {'braca1_x_braca2': (braca1, braca2),
+          'braca1_x_not_braca1': (braca1, not_braca1),
+          'braca2_x_not_braca2': (braca2, not_braca2)}
 
-for gene1, gene2 in [(gene1, gene2) for gene1 in range(n_genes) for gene2 in range(gene1, n_genes)]:
-
-    d1_a = [-1*float(d) for d in braca2[gene1]]
-    d1_b = [float(d) for d in not_braca2[gene1]]
-    d2_a = [-1*float(d) for d in braca2[gene2]]
-    d2_b = [float(d) for d in not_braca2[gene2]]
-
-    b = matrix(-1*np.ones(len(d1_a + d1_b)))
-    A = matrix([d1_a + d1_b,
-                d2_a + d2_b,
-                list(np.concatenate((-1*np.ones(len(d1_a)), np.ones(len(d1_b)))))])
-    c = matrix(np.zeros(3))
-
-    try:
-        s = solvers.lp(c, A, b)
-    except:
-        continue
-
-    if s['x']:
-        lin_sep += [(gene1, gene2)]
-
-
-output = open('line_sep_braca2_x_not_braca2', 'wb')
-pickle.dump(lin_sep, output)
+for gname, group_pair in groups.items():
+    group1, group2 = group_pair
+    lin_sep = []
+    for gene1, gene2 in [(gene1, gene2) for gene1 in range(n_genes) for gene2 in range(gene1, n_genes)]:
+        d1_a = [-1*float(d) for d in group1[gene1]]
+        d1_b = [float(d) for d in group2[gene1]]
+        d2_a = [-1*float(d) for d in group1[gene2]]
+        d2_b = [float(d) for d in group2[gene2]]
+        b = matrix(-1*np.ones(len(d1_a + d1_b)))
+        A = matrix([d1_a + d1_b,
+                    d2_a + d2_b,
+                    list(np.concatenate((-1*np.ones(len(d1_a)), np.ones(len(d1_b)))))])
+        c = matrix(np.zeros(3))
+    
+        try:
+            s = solvers.lp(c, A, b)
+        except:
+            continue
+    
+        if s['x']:
+            lin_sep += [(gene1, gene2)]
+    
+    output = open('lin_sep.{}'.format(gname), 'wb')
+    pickle.dump(lin_sep, output)
--- pattern_recognition/find_solutions.py
+++ pattern_recognition/find_solutions.py
@@ -3,36 +3,37 @@ from cvxopt import matrix, solvers
 import numpy as np
 import pickle
 
-csv_data = pd.read_csv('./data_breast_cancer.csv', header=False)
-data = csv_data.T
-
+data = pd.read_csv('./data_breast_cancer.csv', header=False).T
 braca1 = data[data[3226] == 'BRACA1']
 braca2 = data[data[3226] == 'BRACA2']
 not_braca1 = data[data[3226] != 'BRACA1'][1:]
 not_braca2 = data[data[3226] != 'BRACA2'][1:]
-solutions = {}
-group1 = braca2
-group2 = not_braca2
 P = matrix(np.diag([1.0,1,0]))
 q = matrix([0.0]*3)
-n_genes = 3226
-genes = pickle.load(open('line_sep_braca2_x_not_braca2', 'rb'))
+groups = {'braca1_x_braca2': (braca1, braca2),
+          'braca1_x_not_braca1': (braca1, not_braca1),
+          'braca2_x_not_braca2': (braca2, not_braca2)}
 
-for gene1, gene2 in genes:
-    b1 = np.array([map(float, b) for b in zip(group1[gene1], group1[gene2])])
-    b2 = np.array([map(float, b) for b in zip(group2[gene1], group2[gene2])])
-    h = matrix(-1*np.ones(b1.shape[0] + b2.shape[0]))
-    G1 = np.column_stack([b1, np.ones(b1.shape[0])])
-    G2 = -1.0*np.column_stack([b2, np.ones(b2.shape[0])])
-    G = matrix(np.vstack([G1, G2]))
-    try:
-        sol = solvers.qp(P, q, G, h)
-    except:
-        pass
+for gname, group_pair in groups.items():
+    group1, group2 = group_pair
+    genes = pickle.load(open('lin_sep.{}'.format(gname), 'rb'))
+    solutions = {}
+    for gene1, gene2 in genes:
+        b1 = np.array([map(float, b) for b in zip(group1[gene1], group1[gene2])])
+        b2 = np.array([map(float, b) for b in zip(group2[gene1], group2[gene2])])
+        h = matrix(-1*np.ones(b1.shape[0] + b2.shape[0]))
+        G1 = np.column_stack([b1, np.ones(b1.shape[0])])
+        G2 = -1.0*np.column_stack([b2, np.ones(b2.shape[0])])
+        G = matrix(np.vstack([G1, G2]))
 
-    if sol['status'] == 'optimal':
-        w = np.array(sol['x']).reshape((1,3))[0]
-        w_mod = np.sqrt(w.T.dot(w))
-        solutions[(gene1, gene2)] = [w, w_mod]
-
-pickle.dump(solutions, open('solutions.braca2_x_not_braca2', 'wb'))
+        try:
+            sol = solvers.qp(P, q, G, h)
+        except:
+            pass
+    
+        if sol['status'] == 'optimal':
+            w = np.array(sol['x']).reshape((1,3))[0]
+            w_mod = np.sqrt(w.T.dot(w))
+            solutions[(gene1, gene2)] = [w, w_mod]
+    
+    pickle.dump(solutions, open('solutions.{}'.format(gname), 'wb'))
